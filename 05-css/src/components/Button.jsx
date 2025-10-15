bc9f16bf96b111c6e585dad7244b087a68038567
Abhishek@7781 <abhisheksingh5208@gmail.com>
2025-10-15 23:47:16 +0530

    learn how globle css write

---
+++ 05-css/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+        <button className='btn'>Click me</button>
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-css/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+         Sing is  King 
+         <button className='btn'>
+            click me
+         </button>
+    </div>
+  )
+}
+
+export default Header
