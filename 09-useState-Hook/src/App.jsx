e3197981c64d55896471129d3319a64370d1af78
Abhishek@7781 <abhisheksingh5208@gmail.com>
2025-10-17 04:06:59 +0530

    makeing counter using useState

--- 09-useState-Hook/src/App.jsx
+++ 09-useState-Hook/src/App.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
 
 const App = () => {
-    const [num, setnum] = useState(20)
-    const [user, setuser] = useState("golu")
-    const [users, setUsers] = useState(["rahul"," ","vikash"," ","ramu"])
+   const [count, setcount] = useState(0)
   return (
-    <div className='m-20  '>
-        <h1>Hello i am {user} and my age is {num} <br/> and my friend name is {users} </h1>
-        <button className='bg-gray-500 px-1 py-2 rounded  ' onClick={()=>{ setnum(100),setuser("Abhishek") , setUsers(["Alok"," "," Akash"])}}>Click me </button>
+    <div>
+         <h1 className=' p-3 bg-gray-800 text-white m-7 ml-10 p-3 rounded size-fit'>{count}</h1>
+        <button className='px-2 py-3  bg-gray-500 rounded' onClick={()=>{setcount(count+1)}} >incress</button>
+        <button className='px-2 py-3 m-5 bg-gray-500 rounded'onClick={()=>{setcount(count-1)}} >decress</button>
     </div>
   )
-
 }
 
 export default App
