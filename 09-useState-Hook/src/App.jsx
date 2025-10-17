56bec0ad4d34ba9a397c098ca31b7dd40bc247a9
Abhishek@7781 <abhisheksingh5208@gmail.com>
2025-10-18 00:56:04 +0530

    event heandling using useState

--- 09-useState-Hook/src/App.jsx
+++ 09-useState-Hook/src/App.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 
 const App = () => {
-   const [count, setcount] = useState(0)
+
+   const [num, setnum] = useState(10)
+
+ const btnClicked=()=>{
+   const newNum={...num}
+
+   setnum(prev=>({...prev,user:"alex",age:21}))
+    
+ }
   return (
     <div>
-         <h1 className=' p-3 bg-gray-800 text-white m-7 ml-10 p-3 rounded size-fit'>{count}</h1>
-        <button className='px-2 py-3  bg-gray-500 rounded' onClick={()=>{setcount(count+1)}} >incress</button>
-        <button className='px-2 py-3 m-5 bg-gray-500 rounded'onClick={()=>{setcount(count-1)}} >decress</button>
+      <h1>{num.user} and age {num.age}</h1>
+      <button className='bg-pink-500 p-2 rounded m-5' onClick={btnClicked}>click me</button>
     </div>
   )
 }
