fccd543d432b722bd19705a17c48b85da9d9abea
Abhishek@7781 <abhisheksingh5208@gmail.com>
2025-11-07 14:38:45 +0530

    folder structure done

---
+++ Project III/Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import axios from './utils/Axios'
+import React, { useEffect } from 'react'
+
+const App = () => {
+  const getProduct=async()=>{
+    try {
+      const res=await axios.get("/products")
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+useEffect(()=>{
+  getProduct()
+},[])
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
